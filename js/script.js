a2defbfa7b8ea8eb0e42173bd5aff1f70b0cfa7c
max4eh0v <max4eh0v@gmail.com>
2017-02-19 20:37:34 +0300

    вставил карту(нормально)

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 var link = document.querySelector(".feedback > .btn");
           
-          var popup = document.querySelector(".feedback-form-section");
+var popup = document.querySelector(".feedback-form-section");
           
-          var close = popup.querySelector(".feedback-cross");
+var close = popup.querySelector(".feedback-cross");
           
-          link.addEventListener("click", function(event) {
-            event.preventDefault();
-            popup.classList.add("feedback-form-section-show");
-            name.focus();
-          });
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("feedback-form-section-show");
+});
           
-          close.addEventListener("click", function(event) {
-            event.preventDefault();
-            popup.classList.remove("feedback-form-section-show");
-          });
+close.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("feedback-form-section-show");
+});
