6c8825225febb813eb725541d665426fe88a3c7f
max4eh0v <max4eh0v@gmail.com>
2017-02-22 22:32:50 +0300

    .

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 var link = document.querySelector(".feedback > .btn");
-          
+
 var popup = document.querySelector(".feedback-form-section");
-          
+
 var close = popup.querySelector(".feedback-cross");
-          
+
 link.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.add("feedback-form-section-show");
 });
-          
+
 close.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.remove("feedback-form-section-show");
